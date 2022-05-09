4d6a1ba645e851d17a0127c896ce5b7f8b8037f5
Thiago Mesquita <thyagomesk@gmail.com>
2022-05-09 14:14:36 -0300

    update == 3/**

--- thigasbot/commands/help.js
+++ thigasbot/commands/help.js
@@ -1,18 +1,19 @@
 
 const Discord = require("discord.js")
-const { twitter, author, prefix, versao } = require('../config.json')
+const { twitter, author, prefix, versao, github } = require('../config.json')
 
 exports.run = async (client, message, args) =>{
     const server = message.guild.name
     const embed = new Discord.MessageEmbed()
     .setColor('#0000ff')
-    .setTitle(`Comandos disponíveis atualmente para ${server}`)
+    .setTitle(`Comandos disponíveis atualmente para ${server}\n **ESTE BOT SERÁ ENCERRADO NO DIA 10/10/2022**`)
     .addField('Versão atual:', `${versao}`)
     .addField('Comandos de moderação:', '`antiraid`, `prune`')
     .addField('Outros comandos:', '`avatar`, `ping`, `ideia`, `info`')
     .setDescription(`Ajuda e descrição dos comandos do servidor\n o prefixo deste servidor é ${prefix}.\n Todos os comandos estão temporariamente desativados!`)
-    .addField('Twitter:', `[Siga](https://twitter.com/${twitter})`)
-    .setFooter(`Aguarde ${author} desenvolver outros comandos. Siga ${twitter} no Twitter`)
+    .addField('Twitter:', `[Twitter](https://twitter.com/${twitter})`)
+    .addField('Github:', `[Github](https://github.com/${github})`)
+    .setFooter(`Aguarde ${author} desenvolver outros comandos. Siga ${github} no Github`)
     .setTimestamp()
     await message.channel.send(embed)
 } 
--- thigasbot/commands/info.js
+++ thigasbot/commands/info.js
@@ -4,10 +4,9 @@ exports.run = async (client, message, args) =>{
         const server = message.guild.name
         const embed = new Discord.MessageEmbed()
     .setColor('#6f0b0b')
-    .setTitle(`Todos os comandos atualmente estão desativados. Bot exclusivo de ${server}`)
+    .setTitle(`Todos os comandos atualmente estão desativados. Bot exclusivo de ${server}\n **ESTE BOT SERÁ ENCERRADO NO DIA 10/10/2022**`)
     .setDescription(`Digite **${prefix}help** para saber os comandos atuais deste servidor`)
     .addField('Dev:', `${author}`)
-    .addField('Desenvolvido com:', 'discord.js')
     .addField('Versão:', `${versao}`)
     .addField('For more info, access:', `[Github](https://github.com/${github})`)
     .addField('Twitter:', `[Siga](https://twitter.com/${twitter})`)
